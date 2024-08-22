f7b6e810ff7cb609151de5991ad9a3f69281ab91
Rajiv Ranjan Singh <rajivperfect007@gmail.com>
2024-08-23 04:26:44 +0530

    chore: fix url
    Signed-off-by: Rajiv Ranjan Singh <rajivperfect007@gmail.com>

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://github.com/ramanujansociety/ramanujansociety.github.io',
+  url: 'https://ramanujansociety.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
